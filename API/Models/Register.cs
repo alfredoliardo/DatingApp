54eb21bcbd10b268042df819b5ed045e4bd3ecb2
alfredo.liardo <alfredo.liardo@gmail.com>
2020-11-03 20:37:47 +0100

    end of section 7

--- API/Models/Register.cs
+++ API/Models/Register.cs
@@ -7,6 +7,8 @@ namespace API.Models
         [Required]
         public string UserName { get; set; }
         [Required]
+        [StringLength(16, MinimumLength = 4)]
+        [DataType(DataType.Password)]
         public string Password {get;set;}
     }
 }
