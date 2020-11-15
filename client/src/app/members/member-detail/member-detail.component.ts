ae069dd31e59d3ffb1121e93238098da6a7fac00
alfredo.liardo <alfredo.liardo@gmail.com>
2020-11-15 16:30:53 +0100

    end of section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  public member:Member;
+  public galleryOptions:NgxGalleryOptions[];
+  public galleryImages:NgxGalleryImage[];
 
-  constructor() { }
+  constructor(private memberService: MemberService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [{
+      width: '500px',
+      height: '500px',
+      imagePercent: 100,
+      thumbnailsColumns: 4,
+      preview: false,
+      imageAnimation: NgxGalleryAnimation.Slide
+    }];
+
+    
+  }
+
+  getImages(){
+    const imageUrls = [];
+    for(const photo of this.member.photos){
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      });
+
+    }
+    return imageUrls;
+  }
+
+  loadMember(){
+    let userName:string = this.route.snapshot.paramMap.get('username');
+    console.log(userName);
+    this.memberService.getMember(userName)
+      .subscribe(m => {
+        this.member = m;
+        this.galleryImages = this.getImages();
+      });
   }
 
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent implements OnInit {
 
-  constructor() { }
+export class MemberListComponent implements OnInit {
+  members:Member[];
+  constructor(private memberService:MemberService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers(){
+    this.memberService.loadMembers()
+      .subscribe(members => this.members = members);
+  }
 }
